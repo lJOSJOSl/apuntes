900d576de4d32888a775b9a374dc1a8651859ed8
Joseph Irving Velazquez Noya <267204236+lJOSJOSl@users.noreply.github.com>
2026-08-20 17:06:43 -0600

    Primer commit archivos de clase

---
+++ 01_print.py
@@ -0,0 +1,16 @@
+###
+# 01 - print()
+# El módulo print() es el módilo que nos permite imprimir en consola
+# Sirve para mostrar informacion en consola y te va a acompañar
+# TODA TU VIDA. Desde hoy hasta el fin de los tiempos.
+###
+print("¡Hola, Twitch!")
+
+print('Esto tambien funciona con una comilla')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep="-")
+
+print("Esto se imprime", end = " ")
+print("en una linea")
---
+++ 02_types.py
@@ -0,0 +1,23 @@
+###
+# 01 - types()
+# Python tiene varios tipos de datos 
+# int, float, complex, str, bool, Nonetype, list, tuple, dict, range, set....
+###
+
+print("int:")
+# print(type(10))
+# print(type(0))
+# print(type(-5))
+# print(type(564535456456))
+
+# print(564535456456)
+# print(564535456456 + 1)
+
+print("float:")
+# print(type(3.14))
+# print(type(1.0))
+# print(type(1e3))
+
+print("complex:")
+#print(1 + 2j)
+#print(type(1 + 2j))
